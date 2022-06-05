d7bdccc1679921eda9010d52236409f80460bac3
cglotr <amir.ariffin.1992@gmail.com>
2022-06-06 05:44:53 +0800

    add tests

--- kifu.go
+++ kifu.go
@@ -2,28 +2,26 @@ package kifu
 
 import (
 	"fmt"
-	"log"
-)
-
-const (
-	ERROR = "ERRR"
-	FATAL = "FATL"
-	INFO  = "INFO"
-	WARN  = "WARN"
+	"os"
+	"strings"
 )
 
 func Info(format string, a ...any) {
-	log.Printf("%v %v\n", INFO, fmt.Sprintf(format, a...))
+	fmt.Printf("٩(｡•́‿•̀｡)۶ · %v\n", fmt.Sprintf(format, a...))
 }
 
 func Warn(format string, a ...any) {
-	log.Printf("%v %v\n", WARN, fmt.Sprintf(format, a...))
+	fmt.Printf("(；⌣̀_⌣́) · %v\n", fmt.Sprintf(format, a...))
 }
 
 func Error(format string, a ...any) {
-	log.Printf("%v %v\n", ERROR, fmt.Sprintf(format, a...))
+	fmt.Printf("(╬ Ò﹏Ó) · %v\n", fmt.Sprintf(format, a...))
 }
 
 func Fatal(format string, a ...any) {
-	log.Fatalf("%v %v\n", FATAL, fmt.Sprintf(format, a...))
+	fmt.Printf("凸(￣ヘ￣) · %v\n", fmt.Sprintf(format, a...))
+	if strings.HasSuffix(os.Args[0], "test") {
+		return
+	}
+	os.Exit(1)
 }
